8cbaeefe92b7e36d6ffc43837cb6b35195eee42e
ChillCode <9787055+ChillCode@users.noreply.github.com>
2024-01-21 16:03:44 +0100

    Update version and change plugin name

--- wc-untappd-ratings.php
+++ wc-untappd-ratings.php
@@ -1,27 +1,28 @@
 <?php
 /**
- * Plugin Name: WooCommerce Untappd Ratings
+ * Plugin Name: Untappd Ratings for WooCommerce
  * Plugin URI: https://github.com/chillcode/wc-untappd-ratings
  * Documentation URI: https://github.com/chillcode/wc-untappd-ratings
  * Description: Connect your WooCommerce Store with Untappd
  * Author: ChillCode
  * Author URI: https://github.com/chillcode/
- * Version: 1.0.2
+ * Version: 1.0.4
  * Text Domain: wc-untappd-ratings
  * Domain Path: /languages/
+ * php version 8.0
  *
  * Copyright: (c) 2003-2022 ChillCode (https://github.com/chillcode/)
  *
  * @author    ChillCode
  * @copyright Copyright (c) 2003-2022, ChillCode (https://github.com/chillcode/)
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
- * @package   WooCommerce Untappd Ratings
+ * @package   Untappd Ratings for WooCommerce
  *
  * Domain Path: /languages/
- * Requires at least: 5.6
- * Requires PHP: 7.3
+ * Requires at least: 6.0
+ * Requires PHP: 8.0
  * WC requires at least: 6.0.1
- * WC tested up to: 6.8.0
+ * WC tested up to: 8.1
  */
 
 // Exit if accessed directly.
@@ -30,7 +31,7 @@ defined( 'ABSPATH' ) || exit;
 // Define global constants.
 define( 'WC_UNTAPPD_RATINGS_PLUGIN_FILE', __FILE__ );
 define( 'WC_UNTAPPD_RATINGS_PLUGIN_DIR', dirname( WC_UNTAPPD_RATINGS_PLUGIN_FILE ) . DIRECTORY_SEPARATOR );
-define( 'WC_UNTAPPD_RATINGS_VERSION', '1.0.2' );
+define( 'WC_UNTAPPD_RATINGS_VERSION', '1.0.4' );
 
 require_once WC_UNTAPPD_RATINGS_PLUGIN_DIR . 'classes' . DIRECTORY_SEPARATOR . 'class-wc-untappd-ratings.php';
 
