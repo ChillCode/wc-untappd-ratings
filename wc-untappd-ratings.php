b7af27544a9495a69345f7f1dfc81f50989219b3
ChillCode <9787055+ChillCode@users.noreply.github.com>
2024-02-09 12:03:31 +0100

    cUpdate version and remove GLOBAL.

--- wc-untappd-ratings.php
+++ wc-untappd-ratings.php
@@ -11,10 +11,10 @@
  * Domain Path: /languages/
  * php version 8.0
  *
- * Copyright: (c) 2003-2022 ChillCode (https://github.com/chillcode/)
+ * Copyright: (c) 2003-2024 ChillCode (https://github.com/chillcode/)
  *
  * @author    ChillCode
- * @copyright Copyright (c) 2003-2022, ChillCode (https://github.com/chillcode/)
+ * @copyright Copyright (c) 2003-2024, ChillCode (https://github.com/chillcode/)
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  * @package   Untappd Ratings for WooCommerce
  *
@@ -22,7 +22,7 @@
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * WC requires at least: 6.0.1
- * WC tested up to: 8.1
+ * WC tested up to: 8.5.1
  */
 
 // Exit if accessed directly.
@@ -31,8 +31,8 @@ defined( 'ABSPATH' ) || exit;
 // Define global constants.
 define( 'WC_UNTAPPD_RATINGS_PLUGIN_FILE', __FILE__ );
 define( 'WC_UNTAPPD_RATINGS_PLUGIN_DIR', dirname( WC_UNTAPPD_RATINGS_PLUGIN_FILE ) . DIRECTORY_SEPARATOR );
-define( 'WC_UNTAPPD_RATINGS_VERSION', '1.0.4' );
+define( 'WC_UNTAPPD_RATINGS_VERSION', '1.0.5' );
 
 require_once WC_UNTAPPD_RATINGS_PLUGIN_DIR . 'classes' . DIRECTORY_SEPARATOR . 'class-wc-untappd-ratings.php';
 
-$GLOBALS['WC_Untappd_Ratings'] = WC_Untappd_Ratings();
+WC_Untappd_Ratings();
