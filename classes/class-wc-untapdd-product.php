b990ee3842ba77c70a495e8b22a228956fcf144a
ChillCode <9787055+ChillCode@users.noreply.github.com>
2024-02-13 00:43:15 +0100

    Add update_beer_meta

--- classes/class-wc-untapdd-product.php
+++ classes/class-wc-untapdd-product.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2022 ChillCode
+ * Copyright (C) 2024 ChillCode
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,9 +13,9 @@
  * GNU General Public License for more details.
  *
  * @author    ChillCode
- * @copyright Copyright (c) 2022, ChillCode All rights reserved.
+ * @copyright Copyright (c) 2024, ChillCode All rights reserved.
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
- * @package   WooCommerce Untappd
+ * @package   Untappd Ratings for WooCommerce
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -39,9 +39,7 @@ class WC_Untapdd_Product {
 			add_filter( 'woocommerce_structured_data_context', __CLASS__ . '::woocommerce_structured_data_context', 10, 4 );
 		}
 
-		$untappd_ratings_allow = get_option( 'wc_untappd_ratings_allow', 'no' ) === 'yes' && WC_Untappd_Ratings::api_is_active() ? true : false;
-
-		if ( $untappd_ratings_allow ) {
+		if ( wc_untappd_ratings_enabled() ) {
 			add_filter( 'woocommerce_get_catalog_ordering_args', array( $this, 'woocommerce_get_catalog_ordering_args' ), 998 );
 			add_filter( 'posts_clauses', array( $this, 'posts_clauses' ), 999, 2 );
 		}
@@ -59,7 +57,7 @@ class WC_Untapdd_Product {
 
 		switch ( $orderby ) {
 			case '_untappd_average_rating':
-				$order                    = $wp_query->get( 'order' ) ? esc_sql( $wp_query->get( 'order' ) ) : 'desc';
+				$order                    = esc_sql( $wp_query->get( 'order', 'desc' ) );
 				$posts_clauses['orderby'] = " wp_postmeta.meta_value+0 {$order}, wp_posts.ID {$order} ";
 				break;
 		}
@@ -117,7 +115,7 @@ class WC_Untapdd_Product {
 				'id'          => 'untappd_beer_id',
 				'label'       => esc_html__( 'Untappd beer ID', 'wc-untappd-ratings' ),
 				'placeholder' => '',
-				'value'       => ( ! empty( $untappd_beer_id ) ? $untappd_beer_id : '' ),
+				'value'       => ( ! empty( $untappd_beer_id ) ? esc_attr( $untappd_beer_id ) : '' ),
 				'desc_tip'    => 'true',
 				'description' => esc_html__( 'Enter untappd beer ID', 'wc-untappd-ratings' ),
 				'type'        => 'number',
@@ -139,13 +137,15 @@ class WC_Untapdd_Product {
 		}
 
 		$woocommerce_untappd_nonce = filter_input( INPUT_POST, 'woocommerce_untappd_nonce', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
-		// PHPCS:disable WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+
 		if ( ! $woocommerce_untappd_nonce || ! wp_verify_nonce( $woocommerce_untappd_nonce, 'woocommerce_untappd_nonce' ) || ! current_user_can( 'edit_post', $post_id ) ) {
 			return;
 		}
 
-		if ( isset( $_POST['untappd_beer_id'] ) ) {
-			update_post_meta( $post_id, '_untappd_beer_id', absint( $_POST['untappd_beer_id'] ) );
+		$woocommerce_untappd_beer_id = filter_input( INPUT_POST, 'untappd_beer_id', FILTER_VALIDATE_INT );
+
+		if ( $woocommerce_untappd_beer_id ) {
+			update_post_meta( $post_id, '_untappd_beer_id', absint( $woocommerce_untappd_beer_id ) );
 		}
 	}
 
@@ -160,11 +160,16 @@ class WC_Untapdd_Product {
 	public static function woocommerce_structured_data_context( $array, $data, $type, $value ) {
 
 		if ( 'product' === $type && WC_Untappd_Ratings::api_is_active() ) {
+			/**
+			 * WC_Product
+			 *
+			 * @var WC_Product $product
+			 */
 			global $product;
 
-			$beer_id = (int) $product->get_meta( '_untappd_beer_id', true );
+			$beer_id = absint( $product->get_meta( '_untappd_beer_id', true ) );
 
-			if ( $beer_id <= 0 ) {
+			if ( 0 === $beer_id ) {
 				return $array;
 			}
 
@@ -172,7 +177,7 @@ class WC_Untapdd_Product {
 
 			if ( isset( $beer_info['response']['beer']['rating_count'] ) && isset( $beer_info['response']['beer']['rating_score'] ) ) {
 				$rating_value = round( (float) $beer_info['response']['beer']['rating_score'], 2 );
-				$review_count = (int) $beer_info['response']['beer']['rating_count'];
+				$review_count = absint( $beer_info['response']['beer']['rating_count'] );
 
 				if ( $rating_value > 0 && $review_count > 0 ) {
 					$array['aggregateRating']['@type']       = 'AggregateRating';
@@ -181,9 +186,9 @@ class WC_Untapdd_Product {
 				}
 
 				$array['brand']['@type'] = 'Brand';
-				$array['brand']['name']  = (string) isset( $beer_info['response']['beer']['brewery']['brewery_name'] ) ? esc_attr( $beer_info['response']['beer']['brewery']['brewery_name'] ) : get_bloginfo( 'name' );
+				$array['brand']['name']  = (string) isset( $beer_info['response']['beer']['brewery']['brewery_name'] ) ? esc_attr( $beer_info['response']['beer']['brewery']['brewery_name'] ) : esc_attr( get_bloginfo( 'name' ) );
 
-				$array['MPN'] = $product->get_sku();
+				$array['MPN'] = esc_attr( $product->get_sku() );
 
 				if ( empty( $array['MPN'] ) ) {
 					$array['MPN'] = 'Untappd' . $beer_id;
@@ -192,20 +197,21 @@ class WC_Untapdd_Product {
 				if ( ! empty( $beer_info['response']['beer']['checkins'] ) ) {
 					$untappd_checkins = $beer_info['response']['beer']['checkins'];
 
-					if ( $untappd_checkins['count'] > 1 ) {
+					if ( (int) $untappd_checkins['count'] > 1 ) {
 						foreach ( $untappd_checkins['items'] as $checkin ) {
 							$checkin_rating_score                  = round( (float) $checkin['rating_score'], 2 );
-							$checkin_rating_score_minimum_required = (float) get_option( 'wc_untappd_ratings_review_min', 3.5 );
+							$checkin_rating_score_minimum_required = (float) get_option( 'wc_untappd_ratings_review_min', 0 );
 							if ( ! empty( $checkin['checkin_comment'] ) && $checkin_rating_score >= $checkin_rating_score_minimum_required && $rating_value > 0 && $review_count > 0 ) {
+
 								$review_author = trim( $checkin['user']['first_name'] );
-								$review_author = ( $review_author ) ? $review_author : 'Unknown Author';
+								$review_author = ( $review_author ) ? esc_attr( $review_author ) : 'Unknown Author';
 
 								$review_date = gmdate( 'Y-m-d', strtotime( $checkin['created_at'] ) );
 
 								$review_description = trim( $checkin['checkin_comment'] );
 								$review_description = ( $review_description ) ? esc_html( $review_description ) : 'Untappd Rating';
 
-								$review_name = esc_attr( $checkin['beer']['beer_name'] );
+								$review_beer_name = esc_attr( $checkin['beer']['beer_name'] );
 
 								$array['review'][] = array(
 									'@type'         => 'Review',
@@ -215,7 +221,7 @@ class WC_Untapdd_Product {
 									),
 									'datePublished' => $review_date,
 									'description'   => $review_description,
-									'name'          => $review_name,
+									'name'          => $review_beer_name,
 									'reviewRating'  => array(
 										'@type'       => 'Rating',
 										'bestRating'  => '5',
@@ -232,4 +238,44 @@ class WC_Untapdd_Product {
 
 		return $array;
 	}
+
+	/**
+	 * This method will allow you to see extended information about a beer.
+	 *
+	 * @param int $beer_id (Required) The Beer ID that you want to display checkins.
+	 * @param int $product_id (Required) The Product ID that you want to update with ratings.
+	 * @return array|bool
+	 */
+	public static function update_beer_meta( int $beer_id, int $product_id ) {
+		if ( $beer_id <= 0 || $product_id <= 0 || ! WC_Untappd_Ratings::api_is_active() ) {
+			return false;
+		}
+
+		$cache_key = 'wc_untappd_product_update_meta_' . hash( 'md5', 'beer/info/' . $beer_id . '/' . $product_id . '/' );
+
+		$cache_data = get_transient( $cache_key );
+
+		if ( false !== $cache_data ) {
+			return true;
+		}
+
+		$beer_info = WC_Untappd_Ratings::API()->beer_info( $beer_id );
+
+		if ( isset( $beer_info['response']['beer'] ) ) {
+			$rating_count = ( isset( $beer_info['response']['beer']['rating_count'] ) ) ? absint( $beer_info['response']['beer']['rating_count'] ) : 0;
+			$average      = ( isset( $beer_info['response']['beer']['rating_score'] ) ) ? (float) $beer_info['response']['beer']['rating_score'] : 0;
+			$beer_slug    = ( isset( $beer_info['response']['beer']['beer_slug'] ) ) ? sanitize_text_field( wp_unslash( $beer_info['response']['beer']['beer_slug'] ) ) : '';
+
+			update_post_meta( $product_id, '_untappd_rating_count', $rating_count );
+			update_post_meta( $product_id, '_untappd_average_rating', $average );
+			update_post_meta( $product_id, '_untappd_beer_slug', $beer_slug );
+
+			set_transient( $cache_key, true, WC_Untappd_Ratings::API()->get_cache_time() );
+
+			return true;
+		}
+
+		return false;
+	}
+
 }
