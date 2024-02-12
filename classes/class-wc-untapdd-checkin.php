105e066062f0223e7d195ab9adf23df02d0df421
ChillCode <9787055+ChillCode@users.noreply.github.com>
2024-02-13 00:45:09 +0100

    Update PHPDOC

--- classes/class-wc-untapdd-checkin.php
+++ classes/class-wc-untapdd-checkin.php
@@ -1,23 +1,11 @@
 <?php
 /**
- * Copyright 2021 Chillcode All rights reserved.
+ * WC_Untapdd_Checkin
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author    Chillcode
- * @copyright Copyright (c) 2003-2021, Chillcode All rights reserved.
+ * @author    ChillCode
+ * @copyright Copyright (c) 2024, ChillCode All rights reserved.
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
- * @package   WooCommerce Untappd
+ * @package   Untappd Ratings for WooCommerce
  */
 
 defined( 'ABSPATH' ) || exit;
